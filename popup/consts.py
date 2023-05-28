e3a145d7816a288b7f495316ffa47532fed1c4f6
Jared Skinner <jaredskinner@dropbox.com>
2023-05-28 11:30:04 -0500

    continued moving files around

--- popup/consts.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-import os
-
-MODULE_ROOT = os.path.abspath(os.path.dirname(__file__))
-
-STATE_NOT_RUN = "NOT_RUN"
-STATE_SUCCESS = "SUCCESS"
-STATE_FAILURE = "FAILURE"
-STATE_IGNORED = "IGNORED"
